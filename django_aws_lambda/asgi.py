72644077e79a4287589cbaf3e04205a3c394f15b
vadym-khodak <vaddimart@gmail.com>
2021-08-08 21:05:40 +0300

    fix naming

--- django_aws_lambda/asgi.py
+++ django_aws_lambda/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_react_aws_lambda project.
+ASGI config for django_aws_lambda project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_react_aws_lambda.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_aws_lambda.settings')
 
 application = get_asgi_application()
--- django_aws_lambda/wsgi.py
+++ django_aws_lambda/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_react_aws_lambda project.
+WSGI config for django_aws_lambda project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_react_aws_lambda.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_aws_lambda.production')
 
 application = get_wsgi_application()
