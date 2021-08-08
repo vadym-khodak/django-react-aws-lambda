8fc63b5ef0d0679e3a3a1b35118b5b5f6e00eeaa
vadym-khodak <vaddimart@gmail.com>
2021-08-08 20:18:46 +0300

    temp commit

---
+++ users/models.py
@@ -0,0 +1,10 @@
+# from django.contrib.auth.models import AbstractUser
+# from django.db.models import CharField
+#
+# from django.utils.translation import gettext_lazy as _
+#
+#
+# class User(AbstractUser):
+#     """Default user for Django React AWS."""
+#
+#     name = CharField(_("Name of User"), blank=True, max_length=255)
